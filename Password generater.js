f981f2351a3492aaf4c6dcc679c44b8ad6ed65bd
Harshwardhan A. Jagtap <jagtapharshwardhan2007@gmail.com>
2021-06-19 20:15:13 +0530

    submited all file

---
+++ Password generater.js
@@ -0,0 +1,11 @@
+// wel-come
+console.log(`WEL-COME to Project 1`);
+
+// random number
+let r = 1000 + (10000-1000)*Math.random();
+let ar = Math.round(r);
+let rnd = document.getElementById(`rnd`);
+
+rnd.innerHTML = `<p id="rnp">${ar}</p>`
+
+console.log(rnd,btn);
