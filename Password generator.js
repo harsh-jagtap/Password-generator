d5bc27ef1ab87855b8746789abd1a1dca006f13c
Harshwardhan Jagatp <harshwardhanjagtap2007@gmail.com>
2021-09-03 11:17:33 +0530

    favicon / logo added

--- Password generator.js
+++ Password generator.js
@@ -1,11 +1,21 @@
 // wel-come
 console.log(`WEL-COME to Project 1`);
 
-// random number
-let r = 1000 + (10000-1000)*Math.random();
-let ar = Math.round(r);
-let rnd = document.getElementById(`rnd`);
+// insert
 
-rnd.innerHTML = `<p id="rnp">${ar}</p>`
+function randomNum() {
+    // random number
+    let r = 1000 + (10000 - 1000) * Math.random();
+    let ar = Math.round(r);
+    let rnd = document.getElementById(`rnd`);
 
-console.log(rnd,btn);
+    rnd.innerHTML = `<p id="rnp">${ar}</p>`
+
+    console.log(rnd, btn, button);
+}
+
+let button = document.querySelector("#btn");
+button.addEventListener("click", randomNum)
+
+
+randomNum()
